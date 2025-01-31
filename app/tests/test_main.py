ffd7128c04886c2acb1206ad8679499a7be33b0f
nahiyanmubashshir@gmail.com <nahiyanmubashshir@gmail.com>
2025-01-31 20:53:57 +0600

    main test file modified

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -10,23 +10,3 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "FastAPI with Celery and Docker Compose"}
-
-
-@patch("main.create_task.delay")
-def test_run_task(mock_create_task):
-    mock_create_task.return_value.id = "mock_task_id"
-
-    response = client.post("/tasks/test_data")
-
-    assert response.status_code == 200
-    assert response.json() == {"task_id": "mock_task_id", "status": "Processing"}
-
-
-@patch("main.collection.find")
-def test_get_all_tasks(mock_find):
-    mock_find.return_value = [{"data": "test", "status": "completed"}]
-
-    response = client.get("/tasks/")
-
-    assert response.status_code == 200
-    assert response.json() == {"tasks": [{"data": "test", "status": "completed"}]}
