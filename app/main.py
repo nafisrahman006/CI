5d1046cbe1843d98ab333fb3a26b58fc4cca253d
nafisrahman006 <nafisewubd@gmail.com>
2025-03-01 01:03:03 +0600

    check-3

--- app/main.py
+++ app/main.py
@@ -1,24 +1,52 @@
+# from fastapi import FastAPI
+# from app.tasks import create_task
+# from pymongo import MongoClient
+
+# client = MongoClient("mongodb://mongodb:27017/")
+# db = client["celery_db"]
+# collection = db["task_results"]
+
+# app = FastAPI()
+
+# @app.get("/")
+# def read_root():
+#     return {"message": "FastAPI with Celery and Docker Compose"}
+
+# @app.post("/tasks/{data}")
+# def run_task(data: str):
+#     task = create_task.delay(data)
+#     return {"task_id": task.id, "status": "Processing"}
+
+# @app.get("/tasks/")
+# def get_all_tasks():
+#     tasks = list(collection.find({}, {"_id": 0}))
+#     return {"tasks": tasks}
+
 from fastapi import FastAPI
 from app.tasks import create_task
 from pymongo import MongoClient
 
+# Establish MongoDB connection
 client = MongoClient("mongodb://mongodb:27017/")
 db = client["celery_db"]
 collection = db["task_results"]
 
+# Initialize FastAPI app
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "FastAPI with Celery and Docker Compose"}
 
+
 @app.post("/tasks/{data}")
 def run_task(data: str):
     task = create_task.delay(data)
     return {"task_id": task.id, "status": "Processing"}
 
+
 @app.get("/tasks/")
 def get_all_tasks():
     tasks = list(collection.find({}, {"_id": 0}))
     return {"tasks": tasks}
-
+ 
